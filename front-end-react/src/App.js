f3e28524909f15fff3d7b9a6f2d4b3cfeede652c
Nihar Suryawanshi <nihar.suryawanshi@gmail.com>
2020-09-18 16:21:45 -0500

    Added Basic GUI using React

---
+++ front-end-react/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import logo from './logo.svg';
+import logo from './h1b.jpg';
+import logo1 from './h1b1.png'
+import './App.css';
+//<img src={logo} className="App-logo" alt="logo" />
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src = {logo1} />
+        <h1>H1B Checker</h1>
+        <h3>Coming Soon!</h3>
+        <p>
+          Nihar Suryawanshi | Omkar Sahasrabudhe
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
