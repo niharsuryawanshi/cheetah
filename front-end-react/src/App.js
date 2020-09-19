3b65f081d35522a651afda80c85f3f2dbf88ca05
Nihar Suryawanshi <nihar.suryawanshi@gmail.com>
2020-09-19 18:42:11 -0500

    aded coming soon message

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import logo from './logo.svg';
-import logo from './h1b.jpg';
+//import logo from './h1b.jpg';
 import logo1 from './h1b1.png'
 import './App.css';
 //<img src={logo} className="App-logo" alt="logo" />
@@ -12,7 +12,7 @@ function App() {
       <header className="App-header">
         <img src = {logo1} />
         <h1>H1B Checker</h1>
-        <h3>Coming Soon!</h3>
+        <h3>Website Coming Soon!</h3>
         <p>
           Nihar Suryawanshi | Omkar Sahasrabudhe
         </p>
